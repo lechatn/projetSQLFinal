958332ad8a9022f9ed629b2def3c3341de10a402
lechatn <noe.lechat@ynov.com>
2024-10-23 11:27:42 +0200

    add employe modif

--- src/handlers.go
+++ src/handlers.go
@@ -54,7 +54,8 @@ type hierarchy struct {
 type addEmploye struct {
 	DepartementList []departement
 	PostList []post
-	Sucess bool
+	EmployeList []employes
+	ProjectList []project
 }
 
 func HomeHandler (w http.ResponseWriter, r *http.Request) {
@@ -181,10 +182,63 @@ func AddEmployeHandler (w http.ResponseWriter, r *http.Request) {
 
 	}
 
+	rows6, errQuery6 := db.QueryContext(context.Background(), `SELECT idEmployes, name, firstname FROM employes`)
+
+	if errQuery6 != nil {
+		http.Error(w, "Error with employes table in query", http.StatusInternalServerError)
+		return
+	}
+
+	if rows6 != nil {
+		defer rows6.Close()
+	}
+
+	var employeList []employes
+
+	for rows6.Next() {
+		var employe employes
+		errScan := rows6.Scan(&employe.IdEmployes, &employe.Name, &employe.Firstname)
+		if errScan != nil {
+			http.Error(w, "Error with employes table", http.StatusInternalServerError)
+			return
+		}
+
+		employeList = append(employeList, employe)
+
+	}
+
+
+	rows7, errQuery7 := db.QueryContext(context.Background(), `SELECT idProject, name FROM project`)
+
+	if errQuery7 != nil {
+		http.Error(w, "Error with project table in query", http.StatusInternalServerError)
+		return
+	}
+
+	if rows7 != nil {
+		defer rows7.Close()
+	}
+
+	var projectList []project
+
+	for rows7.Next() {
+		var project project
+		errScan := rows7.Scan(&project.IdProject, &project.Name)
+		if errScan != nil {
+			http.Error(w, "Error with project table", http.StatusInternalServerError)
+			return
+		}
+
+		projectList = append(projectList, project)
+
+	}
+
 	var addemployes addEmploye
 
 	addemployes.DepartementList = departementList
 	addemployes.PostList = postList
+	addemployes.EmployeList = employeList
+	addemployes.ProjectList = projectList
 
 
 
@@ -219,6 +273,8 @@ func SubmitEmployeHandler (w http.ResponseWriter, r *http.Request) {
 	idDepartement := r.FormValue("departement")
 	idPost := r.FormValue("post")
 	salary := r.FormValue("salary")
+	superior := r.FormValue("superior")
+	project := r.FormValue("project")
 
 	idPost = idPost[:1]
 	idDepartement = idDepartement[:1]
@@ -230,7 +286,41 @@ func SubmitEmployeHandler (w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	rows6, errQuery6 := db.QueryContext(context.Background(), `SELECT idEmployes FROM employes WHERE name = ? AND firstname = ?`, name, firstname)
 
+	if errQuery6 != nil {
+		http.Error(w, "Error with employes table in query", http.StatusInternalServerError)
+		return
+	}
+
+	if rows6 != nil {
+		defer rows6.Close()
+	}
+
+	var idEmployes string
+
+	for rows6.Next() {
+		errScan := rows6.Scan(&idEmployes)
+		if errScan != nil {
+			http.Error(w, "Error with employes table", http.StatusInternalServerError)
+			return
+		}
+
+	}
+
+	_, errExec2 := db.ExecContext(context.Background(), "INSERT INTO hierarchy (idEmployes, idSuperior) VALUES (?, ?)", idEmployes, superior)
+
+	if errExec2 != nil {
+		http.Error(w, "Error inserting into hierarchy table", http.StatusInternalServerError)
+		return
+	}
+
+	_, errExec3 := db.ExecContext(context.Background(), "INSERT INTO employes_project VALUES (?, ?)", idEmployes, project)
+
+	if errExec3 != nil {
+		http.Error(w, "Error inserting into employes_project table", http.StatusInternalServerError)
+		return
+	}
 
 	http.Redirect(w, r, "/addemploye", http.StatusSeeOther)
 }
